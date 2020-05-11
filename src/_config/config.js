f8108280f79d2cade7c6185668522a7782c1c193
Thiguet Cabral <thiguet2014@gmail.com>
2020-05-11 19:51:32 -0300

    Changing ws to wss.

--- src/_config/config.js
+++ src/_config/config.js
@@ -1,3 +1,3 @@
 export const QuestionsAPI = 'https://questions-game-server.herokuapp.com/graphql';
 
-export const WSAPI = 'ws://questions-game-ws.herokuapp.com/';
+export const WSAPI = 'wss://questions-game-ws.herokuapp.com/';
